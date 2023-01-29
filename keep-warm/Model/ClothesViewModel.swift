39ad64a4f3053e826bec079f8afdbe7078970b1f
DariaTimofeeva <79119014846@yandex.ru>
2023-01-29 16:53:19 +0300

    added tests

---
+++ keep-warm/Model/ClothesViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ClothesViewModel.swift
+//  keep-warm
+//
+//  Created by Дарья Тимофеева on 18.12.2022.
+//
+
+import Foundation
---
+++ keep-warm/View/ClothesCell.swift
@@ -0,0 +1,12 @@
+//
+//  ClothesCellCollectionViewCell.swift
+//  keep-warm
+//
+//  Created by Екатерина Батеева on 06.12.2022.
+//
+
+import UIKit
+
+class ClothesCell: UICollectionViewCell {
+    
+}
